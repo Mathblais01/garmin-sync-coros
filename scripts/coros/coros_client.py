a53bfb4deb4c0bce8708a549d929ba0da7c994a1
Mathblais01 <119389345+Mathblais01@users.noreply.github.com>
2026-02-16 20:33:10 -0500

    Update coros_client.py

--- scripts/coros/coros_client.py
+++ scripts/coros/coros_client.py
@@ -82,7 +82,7 @@ class CorosClient:
         try:
             data = {
                 "source": 1,
-                "timezone": 32,
+                "timezone": 19,
                 "bucket": bucket,
                 "md5": md5,
                 "size": size,
